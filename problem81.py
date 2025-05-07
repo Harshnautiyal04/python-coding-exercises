6fb0654d2cfcc70d939cfc5f7d0b33681124f893
Harshnautiyal04 <harshnautiyal31@gmail.com>
2025-05-07 11:17:10 +0530

    Find sum of first and last number sum

---
+++ problem81.py
@@ -0,0 +1,34 @@
+# Write a python program to find sum of 1st number  and last number from a list 
+
+# num = int(input("Enter number : "))
+
+# lst = []
+
+# for i in range(num):
+#     lst.append(int(input("Enter number to store in a list : ")))
+
+# first_num = lst[0]
+
+# last_num=lst[-1]
+
+# add = first_num+last_num
+
+# print(f"Addition of {first_num} and {last_num} from a list = {add}.")
+
+# Write a python program to find sum of 2nd number  and 2nd last number from a list 
+
+num = int(input("Enter number : "))
+
+lst = []
+
+for i in range(num):
+    lst.append(int(input("Enter number to store in a list : ")))
+
+second_num = lst[1]
+
+second_last_num=lst[-2]
+
+add = second_num + second_last_num
+
+print(f"Addition of {second_num} and {second_last_num} from a list = {add}.")
+
