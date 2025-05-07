ac1700b6b93a3dc23baeb4a89888ed77531d75f5
Harshnautiyal04 <harshnautiyal31@gmail.com>
2025-05-07 13:18:16 +0530

    Problem84 and its solution

---
+++ problem84.py
@@ -0,0 +1,19 @@
+# write a python program to get name from user to check it is in uppercase or not if not upper then conveert into uppercase 
+
+
+# username = input("Enter username : ")
+
+# if username.isupper():
+#     print("Name is already in uppercase",username)
+# else:
+#     print("Lowercase is converted into uppercase =",username.upper())
+
+# write a python program to get name from user to check it is in lowercase or not if not lower then conveert into lowercase 
+
+
+username = input("Enter username : ")
+
+if username.islower():
+    print("Name is already in Lowercase",username)
+else:
+    print("Uppercase is converted into lowercase =",username.lower())
