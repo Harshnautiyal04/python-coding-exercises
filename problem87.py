174eed727710943104ad39b4eec4426fde0895a2
Harshnautiyal04 <harshnautiyal31@gmail.com>
2025-05-08 11:45:33 +0530

    Problem 87 and its solution

---
+++ problem87.py
@@ -0,0 +1,19 @@
+#write a python program to get a number from a user, the system should add auto increment to that number. Half of user entered number should be incremented 
+
+# num = int(input("Enter a number : "))
+
+# half_num = num/2
+# print("Half number of num =",half_num)
+# num+=half_num
+
+# print("Updated number =",num)
+
+#write a python program to get a number from a user, the system should add auto decrement to that number. Half of user entered number should be decremented 
+
+num = int(input("Enter a number : "))
+
+half_num = num/2
+print("Half number of num =",half_num)
+num-=half_num
+
+print("Updated number =",num)
