70d17c1102e8f6f3318078018057c0316b477ddb
Harshnautiyal04 <harshnautiyal31@gmail.com>
2025-05-08 13:33:45 +0530

    Problem 91 and its solution

--- problem91.py
+++ problem91.py
@@ -1,9 +1,16 @@
 #write a program to find ceiling of a number 
 
+# The ceiling of a number is the smallest integer greater than or equal to that number.
+
+# import math 
+# num = float(input("Enter a number : "))
+# print(math.ceil(num))
+
+#write a program to find ceiling of a number 
+
+#The floor of a number is the largest integer less than or equal to that number.
 
 import math 
 num = float(input("Enter a number : "))
-print(math.ceil(num))
-
 print(math.floor(num))
 
