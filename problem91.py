76796844de8d3988aa1946b323e0c79add1b9e62
Harshnautiyal04 <harshnautiyal31@gmail.com>
2025-05-08 13:40:02 +0530

    Updated file with latest changes

--- problem91.py
+++ problem91.py
@@ -6,7 +6,7 @@
 # num = float(input("Enter a number : "))
 # print(math.ceil(num))
 
-#write a program to find ceiling of a number 
+#write a program to find floor of a number 
 
 #The floor of a number is the largest integer less than or equal to that number.
 
