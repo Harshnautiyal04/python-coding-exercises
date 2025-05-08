2265f08e7ca8dc41cf31b045fb8a48181836324b
Harshnautiyal04 <harshnautiyal31@gmail.com>
2025-05-08 14:03:03 +0530

    Problem 92 and its solution

--- problem92.py
+++ problem92.py
@@ -2,15 +2,33 @@
 
 # take input time and velocity from user 
 
+# u = float(input("Initial velocity (in m/s): "))
+
+
+# v= float(input("Final velocity (in m/s): "))
+
+# t = float(input("Enter time (in s): "))
+
+# if t == 0:
+#     print("Print time cannot be zero.")
+# else:
+#     acceleration = (u-v)/t
+#     print(f"The Acceleration is {acceleration} m/s².")
+
+# write a python program to find time taken by an object having velocity diffrence 'dv' and 'a' acceleration. Get velocity and acceleration from the user.
+
+
 u = float(input("Initial velocity (in m/s): "))
 
 
 v= float(input("Final velocity (in m/s): "))
 
-t = float(input("Enter time (in s): "))
+a = float(input("Enter time (in m/s²): "))
 
-if t == 0:
+dv = u-v
+
+if a == 0:
     print("Print time cannot be zero.")
 else:
-    acceleration = (u-v)/t
-    print(f"The Acceleration is {acceleration} m/s².")
+    t= dv/a
+    print(f"The Acceleration is {t} s.")
