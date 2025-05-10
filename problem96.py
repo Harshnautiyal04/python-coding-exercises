450e951d40071d78ebbba0a2427c9119000457e9
Harshnautiyal04 <harshnautiyal31@gmail.com>
2025-05-10 12:08:45 +0530

    intial commit

--- problem96.py
+++ problem96.py
@@ -1,17 +1,21 @@
 #write a python program to find middle element of list item 
 
-num = int(input("Enter number : "))
+num = int(input("Enter number of elements: "))
 
 lst = []
 
-
 for i in range(num):
-    lst.append(int(input("Enter number to store in a list : ")))
+    lst.append(int(input(f"Enter number {i+1}: ")))
 
 l = len(lst)
 
-half = l/2
-
-middle_elmt = int(half)
-
-print("The middle element of a list is =",middle_elmt)
+if l == 0:
+    print("The list is empty. No middle element.")
+elif l % 2 == 1:
+    middle_index = l // 2
+    print("The middle element of the list is:", lst[middle_index])
+else:
+    middle1 = lst[l//2 - 1]
+    middle2 = lst[l//2]
+    print("The list has even number of elements.")
+    print("The two middle elements are:", middle1, "and", middle2)
