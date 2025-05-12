e53dfa57d2fb034eb8dc2f0127a0e9ffaa189c92
Harshnautiyal04 <harshnautiyal31@gmail.com>
2025-05-12 16:55:08 +0530

    Problem 98 and its solution

---
+++ problem98.py
@@ -0,0 +1,25 @@
+# write a python program to get  only uppercase words from user to store in a list 
+
+# lst = []
+
+# for i in range(10):
+#     w = input("Enter word to store in a list : ")
+#     if (w.isupper()):
+#         lst.append(w)
+#     else:
+#         print("Error! Insert uppercase word.")
+# print(lst)
+
+# write a python program to get  only lowercase words from user to store in a list 
+
+lst = []
+
+for i in range(10):
+    w = input("Enter word to store in a list : ")
+    if (w.islower()):
+        lst.append(w)
+    else:
+        print("Error! Insert lowercase word.")
+print(lst)
+
+
