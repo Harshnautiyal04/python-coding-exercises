bb824492ff21584614195abc27c2e4758ba345fc
Harshnautiyal04 <harshnautiyal31@gmail.com>
2025-05-13 17:55:17 +0530

    Problem 100 and dits solution

--- problem100.py
+++ problem100.py
@@ -6,5 +6,5 @@ for i in range(5):
     lst.append(input("Insert name to store in a list : "))
 
 for i in lst:
-    if not(i.lower().startswith('f')) and (i.endswith('n')):
+    if not(i.lower().startswith('f')):
         print(i)
